573a3ff542b153d4a076c34beb14159ecb7f77aa
Artem <artemmarco@gmail.com>
2022-12-22 14:14:59 +0300

    Home work seminar 3

--- Program.cs
+++ Program.cs
@@ -1,30 +1,50 @@
-﻿/*Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Обязательна проверка на ввод чисел больше/меньше 3-х знаков
-Console.WriteLine("Введи трехзначное число: ");
-int number= int.Parse(Console.ReadLine()!);
-int result = (number/10)%10;
-if(number < 1000 && number > 99) Console.WriteLine($"{number} -> {result}");
-else Console.WriteLine ($"Введенное число не трехзначное");
-*/
+﻿//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-/*Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Берем числа до 100000
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine()!;
+// int len = number.Length;
 
-Console.WriteLine("Введи трехзначное число: ");
-int number= int.Parse(Console.ReadLine()!);
-int result = number%10;
-if(number < 1000 && number > 99) Console.WriteLine($"{number} -> {result}");
-if(number < 100-999 || number > 10000-99999 ) Console.WriteLine($"Некоректно введено число");
-*/
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - НЕ палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+// }
 
-//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Обязательна проверка на ввод числа <1 и >7
+// //Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.WriteLine("Введите номер дня недели: ");
-int num = int.Parse(Console.ReadLine()!);
+// System.Console.Write("Введите коррдинату x точки А: ");
+// int x1 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.Write("Введите коррдинату y точки А: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите координату z точки A: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите коордианту x точки B: ");
+// int x2 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.Write("Введите коордианту y точки B: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите коррдинату x точки А: ");
+// int z2 = Convert.ToInt32(Console.ReadLine()); 
+// System.Console.Write("Введите коордианту z точки B: ");
 
-if (num == 1)Console.WriteLine ($"Для числа {num} соотвествует день недели Понедельник");
-if (num == 2)Console.WriteLine ($"Для числа {num} соотвествует день недели Вторник");
-if (num == 3)Console.WriteLine ($"Для числа {num} соотвествует день недели Среда");
-if (num == 4)Console.WriteLine ($"Для числа {num} соотвествует день недели Четверг");
-if (num == 5)Console.WriteLine ($"Для числа {num} соотвествует день недели Пятница");
-if (num == 6)Console.WriteLine ($"Для числа {num} соотвествует день недели Суббота(Выходной день)");
-if (num == 7)Console.WriteLine ($"Для числа {num} соотвествует день недели Воскресенье(Выходной день)");
-if (num>7 || num<1)Console.WriteLine($"Некоректоно введена, повторите попытку");
+// double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1-y2),2) + Math.Pow((z1-z2),2)),2); 
+// System.Console.WriteLine($"Расстояние между тремя точками: {result}");
+
+//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введи число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= x; i++)
+{
+System.Console.Write(Math.Pow(i, 3)+ " ");
+}
