e140669ba112959aedea196f89c2c47131a1087d
Artem <artemmarco@gmail.com>
2023-01-17 15:16:39 +0300

    Home work seminar 4

--- Program.cs
+++ Program.cs
@@ -41,10 +41,67 @@
 
 //Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-System.Console.WriteLine("Введи число");
-int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введи число");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-for(int i = 1; i <= x; i++)
+// for(int i = 1; i <= x; i++)
+// {
+// System.Console.Write(Math.Pow(i, 3)+ " ");
+// }
+
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (Задачи, решенные через Math.Pow, будут отправлены на переделку, так как задача стоит в том, чтобы написать цикл)
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// Console.WriteLine("введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int step = a;
+
+
+// for (int i = 1; i < b; i++)
+// {
+// step = step * a;
+// }
+
+// Console.WriteLine($"{a} в степени {b} равно: " + step);
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// 
+
+// Console.WriteLine("введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (a > 0)
+// {
+// int num = a % 10;
+// a = a / 10;
+// sum = sum + num;
+// }
+// Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+int[] Array = GetArray(8);
+Console.Write($"[{String.Join(";", Array)}]");
+
+int[] GetArray(int size)
 {
-System.Console.Write(Math.Pow(i, 3)+ " ");
+
+    int[] Array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(3, 10);
+    }
+     return Array;
+
 }
