128dbea3c06103a11be1b2354f9b311e4dbd725e
Artem <artemmarco@gmail.com>
2023-01-26 16:36:48 +0300

    Home work seminar 8

--- Program.cs
+++ Program.cs
@@ -269,53 +269,248 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-int m = 5;
-int n = 5;
-int[,] array = new int[m, n];
+// int m = 5;
+// int n = 5;
+// int[,] array = new int[m, n];
 
-//--------metods-------
+// //--------metods-------
 
-void GetArray(int[,] array)
+// void GetArray(int[,] array)
+// {
+//     for (int m = 0; m < array.GetLength(0); m++)
+//     {
+//         for (int n = 0; n < array.GetLength(1); n++)
+//         {
+//             array[m, n] = new Random().Next(1, 20);
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int m = 0; m < array.GetLength(0); m++)
+//     {
+//         for (int n = 0; n < array.GetLength(1); n++)
+//         {
+//             Console.Write($"{array[m, n]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// string ArithmeticMean(int[,] array)
+// {
+//     string result = string.Empty;
+//     for (int n = 0; n < array.GetLength(1); n++)
+//     {
+//         int sum = 0;
+//         int count = 0;
+//         double mean = 0;
+//         for (int m = 0; m < array.GetLength(0); m++)
+//         {
+//             sum += array[m, n];
+//             count++;
+//         }
+//         mean = sum / count;
+//         result += mean.ToString() + " ";
+//     }
+//     return result;
+// }
+ 
+//  GetArray(array);
+// PrintArray(array);
+// Console.WriteLine();
+// Console.WriteLine(ArithmeticMean(array));
+
+// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+// int[,] matrix = new int[5, 5];
+// GetArray(matrix);
+// PrintArray(matrix);
+// Console.WriteLine();
+// Ordering(matrix);
+// PrintArray(matrix);
+
+// void GetArray(int[,] matr)
+// {
+//     for (int m = 0; m < matr.GetLength(0); m++)
+//     {
+//         for (int n = 0; n < matr.GetLength(1); n++)
+//         {
+//             matr[m, n] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int m = 0; m < matr.GetLength(0); m++)
+//     {
+//         for (int n = 0; n < matr.GetLength(1); n++)
+//         {
+//             Console.Write($"{matr[m, n]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void Ordering(int[,] matr)
+// {
+//     for (int m = 0; m < matr.GetLength(0); m++)
+//     {
+//         for (int n = 0; n < matr.GetLength(1); n++)
+//         {
+//             for (int k = 0; k < matr.GetLength(1) - n - 1; k++)
+//             {
+//                 if (matr[m, k] < matr[m, k + 1])
+//                 {
+//                     int t = matr[m, k];
+//                     matr[m, k] = matr[m, k + 1];
+//                     matr[m, k + 1] = t;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+// int[,] table = new int[4, 4];
+// FillArrayRandom(table);
+// PrintArray(table);
+// Console.WriteLine();
+// NumberRowMinSumElements(table);
+
+
+// void NumberRowMinSumElements(int[,] array)
+// {
+//     int minRow = 0;
+//     int minSumRow = 0;
+//     int sumRow = 0;
+//     for (int i = 0; i < table.GetLength(1); i++)
+//     {
+//         minRow += table[0, i];
+//     }
+//     for (int i = 0; i < table.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < table.GetLength(1); j++) sumRow += table[i, j];
+//         if (sumRow < minRow)
+//         {
+//             minRow = sumRow;
+//             minSumRow = i;
+//         }
+//         sumRow = 0;
+//     }
+//     Console.Write($"{minSumRow + 1} строка с наименьшей суммой элементов");
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArrayRandom(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10+1);
+//         }
+//     }
+// }
+
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+
+GetArrayRandom(array);
+PrintArray2D(array);
+
+Console.WriteLine();
+
+GetArrayRandom(secondArray);
+PrintArray2D(secondArray);
+
+Console.WriteLine();
+
+if (array.GetLength(0) != secondArray.GetLength(1))
 {
-    for (int m = 0; m < array.GetLength(0); m++)
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
     {
-        for (int n = 0; n < array.GetLength(1); n++)
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
         {
-            array[m, n] = new Random().Next(1, 20);
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
         }
     }
 }
-void PrintArray(int[,] array)
+
+PrintArray2D(resultArray);
+
+
+int ReadInt(string message)
 {
-    for (int m = 0; m < array.GetLength(0); m++)
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void GetArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int n = 0; n < array.GetLength(1); n++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[m, n]} ");
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
     }
 }
-string ArithmeticMean(int[,] array)
-{
-    string result = string.Empty;
-    for (int n = 0; n < array.GetLength(1); n++)
-    {
-        int sum = 0;
-        int count = 0;
-        double mean = 0;
-        for (int m = 0; m < array.GetLength(0); m++)
-        {
-            sum += array[m, n];
-            count++;
-        }
-        mean = sum / count;
-        result += mean.ToString() + " ";
-    }
-    return result;
-}
- 
- GetArray(array);
-PrintArray(array);
-Console.WriteLine();
-Console.WriteLine(ArithmeticMean(array));
